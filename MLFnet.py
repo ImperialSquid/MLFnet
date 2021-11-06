4fcf5bbb7f8faf98b7fb5673e416980526e684df
Ben Jaeger <benjaeger99@gmail.com>
2021-11-06 02:52:57 +0000

    FIX: Fix model data flow for shared blocks
    Now no longer evaluating by group since it confused some things, compiled now holds a dict mapping tasks to sequentials
    
    Removed call to test setup but will kepp it around until the model is fully up and running just in case

--- MLFnet.py
+++ MLFnet.py
@@ -1,3 +1,4 @@
+from importlib import import_module
 from typing import Optional, Tuple, Dict
 
 from torch import nn
@@ -8,38 +9,34 @@ from utils import ModelMixin
 class MLFnet(nn.Module, ModelMixin):
     def __init__(self, tasks: Tuple[str, ...] = tuple(), heads: Optional[Dict[str, nn.Module]] = None):
         super().__init__()
-        self.tasks = None
-        self.groups = None
-        self.paths = None
-        self.blocks = None
-        self.load_test_setup()
+        self.tasks = tuple(sorted(tasks))
+        self.groups = {task: self.tasks for task in self.tasks}
+        self.paths = {group: ["_".join(group)] for group in self.groups.values()}
 
-        # self.tasks = tuple(sorted(tasks))
-        #
-        # self.groups = {task: self.tasks for task in self.tasks}
-        #
-        # self.paths = {group: ["_".join(group)] for group in self.groups.values()}
-        #
-        # self.blocks = nn.ModuleDict()
-        # self.blocks["_".join(self.tasks)] = nn.ModuleList([nn.Identity(), nn.Identity(), nn.Identity()])
+        self.blocks = nn.ModuleDict()
+        self.blocks["_".join(self.tasks)] = nn.ModuleList()
+        self.finished = []
 
         if heads is None or sorted(heads.keys()) != sorted(self.tasks):
-            heads = {task: nn.Flatten() for task in self.tasks}
+            heads = {task: nn.ModuleList([nn.Flatten()]) for task in self.tasks}
         self.heads = nn.ModuleDict()
         for task in heads.keys():
             self.heads[task] = heads[task]
 
-        self.compiled_body = None
-        self.compiled_head = None
+        self.compiled = None
         self.compile_model()
 
     def forward(self, x):
+        x = {task: self.compiled[task](x) for task in self.groups}
         return x
 
     def compile_model(self):
-        self.compiled_body = {group: nn.Sequential(*[nn.Sequential(*self.blocks[block])
-                                                     for block in self.paths[group]])
-                              for group in self.paths}
+        compiled_body = {group: nn.Sequential(*[nn.Sequential(*self.blocks[block]) for block in self.paths[group]])
+                         for group in self.paths}
+        compiled_head = {task: nn.Sequential(*self.heads[task]) for task in self.heads}
+
+        self.compiled = {task: nn.Sequential(*[compiled_body[self.groups[task]], compiled_head[task]])
+                         for task in self.groups}
 
     def load_test_setup(self):
         self.tasks = ("a", "b", "c")
@@ -48,6 +45,6 @@ class MLFnet(nn.Module, ModelMixin):
                        "c": ("c",)}
         self.paths = {("a", "b"): ["a_b_c", "a_b"],
                       ("c",): ["a_b_c", "c"]}
-        self.blocks = {"a_b_c": [nn.Identity(), nn.Identity()],
-                       "a_b": [nn.Identity(), nn.Identity()],
-                       "c": [nn.Identity(), nn.Identity()]}
+        self.blocks = {"a_b_c": nn.ModuleList([nn.Flatten(), nn.Flatten()]),
+                       "a_b": nn.ModuleList([nn.Flatten(), nn.Flatten(), nn.Flatten(), nn.Flatten()]),
+                       "c": nn.ModuleList([nn.Flatten(), nn.Flatten(), nn.Flatten()])}
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import torch
+from torch import nn
 
 from MLFnet import MLFnet
 
 
 def main():
     model = MLFnet(tasks=("a", "b", "c"), heads=None)
-    print(model.compiled_body[("a", "b")](torch.zeros(1, 3, 3, 3)))
 
 
 if __name__ == "__main__":
